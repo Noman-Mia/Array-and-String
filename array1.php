dbf28b55ea1f74ce9e7d8599a9061b788cb10e99
Noman Mia <nomanislamnishad@gmail.com>
2024-10-24 14:43:54 +0600

    intro array

--- array1.php
+++ array1.php
@@ -1,1 +1,15 @@
 <?php
+$man = array("noman",23,"khan","faruk");
+
+// var_dump($man); //data type check
+
+ $n=count($man); //check how much value
+ echo"$n";
+
+ for($i = 0; $i <= $n; $i++){
+    echo $man[$i] ."\n";
+ };
+
+ for($i = $n-1; $i >= 0; $i--){
+    echo $man[$i] ."\n";
+ };
