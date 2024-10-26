bb8430830ce6621ed9afbb06cc48cd3af579ddd5
Noman Mia <nomanislamnishad@gmail.com>
2024-10-26 12:11:33 +0600

    copy by value and references

---
+++ array7.php
@@ -0,0 +1,14 @@
+<?php
+//copy by value
+$persons= ["fname"=>"noman","lname"=>"khan"];
+$newPersons= $persons;
+print_r($newPersons);
+
+//copy by references
+function printData(&$persons){
+    $persons["fname"].="changed";
+    print_r($persons);
+};
+printData($persons);
+
+print_r($persons);
